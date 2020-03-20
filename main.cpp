c6a8b9bdf65d6bbae2927878cc4b90ee9707c3fc
Yuriy Samusevich <yuri@kromephotos.com>
2020-03-20 13:39:01 +0500

    [UPD] more putputs

--- main.cpp
+++ main.cpp
@@ -15,6 +15,13 @@ int main()
 {
    vector <vector <double>> v1{{2, 4}, {3, 6}, {5, 7}, {8, 6}, {6, 2}, {4, 9}, {3, 4}, {2, 4}};
    
+   cout << "Ломаная: " << endl;
+   for (unsigned int p=0; p < v1.size(); p++)
+   {
+       cout << "x: " << v1[p][0] << " y: " << v1[p][1] << endl;
+   }
+
+
    double x, y; 
    double x1, y1, x2, y2;
    double a1, b1, c1, a2, b2, c2;
@@ -23,7 +30,7 @@ int main()
    
    double parts = 0;
    
-   for (int first_point_set=0; first_point_set < v1.size()-1; first_point_set++)
+   for (unsigned int first_point_set=0; first_point_set < v1.size()-1; first_point_set++)
    {
       x1 = v1[first_point_set][0];
       y1 = v1[first_point_set][1];
@@ -35,7 +42,7 @@ int main()
       b1 = x2 - x1;
       c1 = x1 * y2 - x2 * y1; 
       
-      for (int second_point_set=0; second_point_set < v1.size()-1; second_point_set++)
+      for (unsigned int second_point_set=0; second_point_set < v1.size()-1; second_point_set++)
       {
          if (first_point_set != second_point_set)
          {
@@ -50,12 +57,12 @@ int main()
             c2 = x3 * y4 - x4 * y3; 
                
             intersection(a1, a2, b1, b2, c1, c2, x, y);
-            cout << "intersection: " << "(" << x << "," << y << ")" << endl;
+            // cout << "intersection: " << "(" << x << "," << y << ")" << endl;
                
             if ((x > x3) and (x < x2))
                parts += 1;
          }
       }
    }
-   cout << "parts: " << parts + 1;
+   cout << "Количество разбиений: " << parts + 1;
 }
